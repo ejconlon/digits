ef8b41737bf27bc66204e03470f1c3437c1f24f5
Eric Conlon <ejconlon@gmail.com>
2016-10-21 09:45:38 -0700

    more argparse

--- digits/main.py
+++ digits/main.py
@@ -8,29 +8,46 @@ from .data import Env
 
 def make_parser():
   parser = argparse.ArgumentParser()
-  subparsers = parser.add_subparsers(dest='op', help='foo')
-  inspect_parser = subparsers.add_parser('inspect', help='bar')
-  inspect_parser.add_argument('--model', required=True, help='baz')
+  subparsers = parser.add_subparsers(dest='op')
+  inspect_parser = subparsers.add_parser('inspect')
+  inspect_parser.add_argument('--model', required=True)
+  train_parser = subparsers.add_parser('train')
+  train_parser.add_argument('--model', required=True)
+  train_parser.add_argument('--train-data', required=True)
+  train_parser.add_argument('--valid-data', required=False)
+  train_parser.add_argument('--test-data', required=False)
+  test_parser = subparsers.add_parser('test')
+  test_parser.add_argument('--model', required=True)
+  test_parser.add_argument('--test-data', required=True)
   return parser
+
+def inspect(env, args):
+  print('inspecting', args.model)
+  model_path = os.path.join(env.logs, args.model, 'model.ckpt')
+  reader = tf.train.NewCheckpointReader(model_path)
+  var_to_shape_map = reader.get_variable_to_shape_map()
+  for key in var_to_shape_map:
+    print('tensor_name:', key)
+    print(reader.get_tensor(key))
+
+def train(env, args):
+  raise Exception("TODO")
+
+def test(env, args):
+  raise Exception("TODO")
+
+ops = {
+  'inspect': inspect,
+  'train': train,
+  'test': test
+}
 
 def main():
   env = Env('.')
   env.assert_ready()
   parser = make_parser()
   args = parser.parse_args()
-  if args.op == "inspect":
-    inspect(env, args.model)
-  else:
-    raise Exception("Unknown op", args.op)
-
-def inspect(env, name):
-  print('inspecting', name)
-  model_path = os.path.join(env.logs, name, 'model.ckpt')
-  reader = tf.train.NewCheckpointReader(model_path)
-  var_to_shape_map = reader.get_variable_to_shape_map()
-  for key in var_to_shape_map:
-    print('tensor_name:', key)
-    print(reader.get_tensor(key))
+  ops[args.op](env, args)
 
 if __name__ == '__main__':
   main()
