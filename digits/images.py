a4a009a3a82de6d21c3164aed301c8c9d8d4b12a
Eric Conlon <ejconlon@gmail.com>
2016-10-27 13:11:16 -0700

    make stuff

--- digits/images.py
+++ digits/images.py
@@ -3,9 +3,35 @@ import random
 import numpy as np
 import skimage.transform
 
+from .common import product
+
 DEFAULT_S=1.03
 DEFAULT_R=0.15
 DEFAULT_T=1.5
+DEFAULT_I=0.5
+
+def img_width(arr):
+  if is_single_img(arr):
+    i = 0
+  else:
+    i = 1
+  assert arr.shape[i] == arr.shape[i + 1]
+  return arr.shape[i]
+
+def img_depth(arr):
+  if len(arr.shape) == 2:
+    return 1
+  else:
+    if len(arr.shape) == 3:
+      i = 2
+    else:
+      assert len(arr.shape) == 4
+      i = 3
+    d = arr.shape[i]
+    if d == 1 or d == 3:
+      return d
+    else:
+      return 1
 
 # is the array a single image or multiple?
 def is_single_img(arr):
@@ -17,7 +43,7 @@ def img_map(f, arr):
   if is_single_img(arr):
     return f(arr)
   else:
-    return np.concatenate([f(arr[i]) for i in range(arr.shape[0])])
+    return np.stack([f(arr[i]) for i in range(arr.shape[0])])
 
 # apply a function for side effects
 def img_effect(f, arr):
@@ -27,10 +53,30 @@ def img_effect(f, arr):
     for i in range(arr.shape[0]):
       f(arr[i])
 
+def img_flatten(arr):
+  assert len(arr.shape) == 4
+  return arr.reshape((arr.shape[0], product(arr.shape[1:])))
+
+def img_flatten(arr):
+  width = img_width(arr)
+  depth = img_depth(arr)
+  volume = width * width * depth
+  return (arr.reshape((-1, volume)), width, depth)
+
+def img_unflatten(arr, width, depth):
+  if depth == 1:
+    return arr.reshape((-1, width, width))
+  else:
+    assert depth == 3
+    return arr.reshape((-1, width, width, depth))
+
 # apply a random transformation to an image
-def rando(img, s=DEFAULT_S, r=DEFAULT_R, t=DEFAULT_T):
+def rando(img, s=DEFAULT_S, r=DEFAULT_R, t=DEFAULT_T, i=DEFAULT_I):
     scale = random.uniform(1/s, s)
     rot = random.uniform(-r, r)
     trans = (random.uniform(-t, t), random.uniform(-t, t))
     tform = skimage.transform.SimilarityTransform(scale=scale, rotation=rot, translation=trans)
-    return skimage.transform.warp(img, tform, mode='symmetric')
+    warped = skimage.transform.warp(img, tform, mode='symmetric')
+    if random.random() < i:
+      warped = 1.0 - warped
+    return warped
