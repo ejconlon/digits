8ad1d966b45a9cf1a75e9947c76b6f46ac258dc3
Eric Conlon <ejconlon@gmail.com>
2016-11-09 15:14:23 -0800

    hog preprocessor

--- digits/preprocessors.py
+++ digits/preprocessors.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.color import rgb2gray
+import skimage.feature
 
 from .common import product
 from .images import img_prepare_all
@@ -9,24 +10,30 @@ def flat_gray(data):
   X = rgb2gray(X)
   X = X.astype(np.float32)
   X = X.reshape((X.shape[0], product(X.shape[1:])))
-  new_data = data._replace(X=X)
-  return new_data
+  return data._replace(X=X)
 
 def gray(data):
   X = data.X
   X = rgb2gray(X)
   X = X.astype(np.float32)
   X = X.reshape((X.shape[0], X.shape[1], X.shape[2], 1))
-  new_data = data._replace(X=X)
-  return new_data
+  return data._replace(X=X)
 
 def color(data):
   X = img_prepare_all(data.X)
+  return data._replace(X=X)
+
+def hog(data):
+  X = data.X
+  X = rgb2gray(X)
+  X = X.astype(np.float32)
+  X = skimage.feature.hog(X, transform_sqrt=True)
   return data._replace(X=X)
 
 PREPROCESSORS = {
     'noop': lambda data: data,
     'flat-gray': flat_gray,
     'gray': gray,
-    'color': color
+    'color': color,
+    'hog': hog
 }
